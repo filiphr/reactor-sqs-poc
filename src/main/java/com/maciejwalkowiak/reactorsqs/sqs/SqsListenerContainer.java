6ba8bed13a30f0dc65e76216a6415363e79efefd
Filip Hrisafov <filip.hrisafov@gmail.com>
2020-06-05 23:23:52 +0200

    Example container with Java completable future

--- src/main/java/com/maciejwalkowiak/reactorsqs/sqs/SqsListenerContainer.java
+++ src/main/java/com/maciejwalkowiak/reactorsqs/sqs/SqsListenerContainer.java
@@ -1,16 +1,17 @@
 package com.maciejwalkowiak.reactorsqs.sqs;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 import com.maciejwalkowiak.reactorsqs.sqs.SqsProperties.ListenerProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import reactor.core.Disposable;
-import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Scheduler;
-import reactor.core.scheduler.Schedulers;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
 import software.amazon.awssdk.services.sqs.model.DeleteMessageResponse;
@@ -20,17 +21,21 @@ import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
 import software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse;
 
 import org.springframework.beans.factory.DisposableBean;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 public class SqsListenerContainer implements DisposableBean {
 	private static final Logger logger = LoggerFactory.getLogger(SqsListenerContainer.class);
+
+	private final Object monitor = new Object();
 	private final String listenerName;
 	private final ListenerProperties listenerProperties;
 	private final SqsAsyncClient sqs;
 	private final Object target;
 	private final Method targetMethod;
 
-	private Disposable disposable;
-	private Scheduler scheduler;
+	private AsyncTaskExecutor asyncTaskExecutor;
+	private Future<?> future;
 
 	private String queueUrl;
 
@@ -43,28 +48,17 @@ public class SqsListenerContainer implements DisposableBean {
 	}
 
 	public void register() {
-		this.scheduler = Schedulers.newBoundedElastic(listenerProperties.getMaxThreads(), listenerProperties.getMaxTasks(), listenerName);
 		this.queueUrl = getQueueUrl();
-		this.disposable = Mono.fromFuture(this::receiveMessages)
-				.doOnNext(response -> logger.info("Received {} messages", response.messages().size()))
-				.flatMapIterable(ReceiveMessageResponse::messages)
-				.repeat()
-				.publishOn(scheduler)
-				.flatMap(m -> Mono.defer(() -> {
-					try {
-						targetMethod.invoke(target, m);
-						if (listenerProperties.getDeletionPolicy().deleteOnSuccess()) {
-							return Mono.fromFuture(() -> deleteMessage(m));
-						} else {
-							return Mono.empty();
-						}
-					}
-					catch (Exception e) {
-						handleError(e, m);
-						return Mono.empty();
-					}
-				}).subscribeOn(scheduler))
-				.subscribe();
+		ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+		threadPoolTaskExecutor.setCorePoolSize(listenerProperties.getMaxThreads());
+		threadPoolTaskExecutor.setMaxPoolSize(listenerProperties.getMaxThreads());
+		threadPoolTaskExecutor.setAllowCoreThreadTimeOut(true);
+		threadPoolTaskExecutor.afterPropertiesSet();
+		this.asyncTaskExecutor = threadPoolTaskExecutor;
+
+		synchronized (monitor) {
+			future = asyncTaskExecutor.submit(new ReceiveMessagesRunnable());
+		}
 	}
 
 	private String getQueueUrl() {
@@ -93,6 +87,24 @@ public class SqsListenerContainer implements DisposableBean {
 		}
 	}
 
+	private void handleSuccess(Message m) {
+		logger.debug("Successfully to handles message: {}", m);
+		if (listenerProperties.getDeletionPolicy().deleteOnSuccess()) {
+			try {
+				deleteMessage(m).get();
+			}
+			catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+	private void submitExecution(Runnable runnable) {
+		synchronized (monitor) {
+			future = asyncTaskExecutor.submit(runnable);
+		}
+	}
+
 	private CompletableFuture<ReceiveMessageResponse> receiveMessages() {
 		logger.info("Fetching messages [{}]", listenerName);
 		return sqs.receiveMessage(ReceiveMessageRequest.builder()
@@ -103,10 +115,74 @@ public class SqsListenerContainer implements DisposableBean {
 				.build());
 	}
 
+	private class ReceiveMessagesRunnable implements Runnable {
+
+		@Override
+		public void run() {
+			receiveMessages()
+					.thenApplyAsync(response -> {
+						List<Message> messages = response.messages();
+						logger.info("Received {} messages", messages.size());
+						return messages;
+					}, asyncTaskExecutor)
+					.exceptionally(throwable -> {
+						logger.error("Failed to receive messages.", throwable);
+						return Collections.emptyList();
+					})
+					.thenCompose(messages -> {
+						Collection<CompletableFuture<Void>> messageInvocations = new ArrayList<>(messages.size());
+
+						for (Message message : messages) {
+							messageInvocations.add(CompletableFuture.runAsync(new InvokeMethodRunnable(message), asyncTaskExecutor));
+						}
+
+						return CompletableFuture.allOf(messageInvocations.toArray(new CompletableFuture[0]));
+					})
+					.handle((unused, throwable) -> {
+						if (throwable != null) {
+							logger.error("Error while handling messages on queue: {}", queueUrl, throwable);
+						}
+						resubmitExecution();
+						return null;
+					});
+		}
+
+		private void resubmitExecution() {
+			submitExecution(this);
+		}
+	}
+
+	private class InvokeMethodRunnable implements Runnable {
+
+		private final Message message;
+
+		private InvokeMethodRunnable(Message message) {
+			this.message = message;
+		}
+
+		@Override
+		public void run() {
+			try {
+				targetMethod.invoke(target, message);
+			}
+			catch (Exception e) {
+				handleError(e, message);
+			}
+			if (listenerProperties.getDeletionPolicy().deleteOnSuccess()) {
+				handleSuccess(message);
+			}
+		}
+	}
+
 
 	@Override
 	public void destroy() throws Exception {
-		disposable.dispose();
-		scheduler.dispose();
+		synchronized (monitor) {
+			if (future != null) {
+				future.cancel(true);
+				future = null;
+			}
+		}
+		((ThreadPoolTaskExecutor) asyncTaskExecutor).destroy();
 	}
 }
